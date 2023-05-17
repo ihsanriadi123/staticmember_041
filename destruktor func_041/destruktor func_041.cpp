048b5b9d763488d335d1401f3688fdf246092e98
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-17 19:52:55 +0700

    mengakhiri data mahasiswa

--- destruktor func_041/destruktor func_041.cpp
+++ destruktor func_041/destruktor func_041.cpp
@@ -25,3 +25,23 @@ angka::angka() { //destructor
 	cout << "alamat array sudah dilepaskan" << endl;
 }
 
+void angka::cetakdata() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = " << arr[i] << endl;
+	}
+}
+
+void angka::isidata() {
+	for (int i = 1; i <= panjang; i++) {
+		cout << i << " = "; cin >> arr[i];
+	}
+	cout << endl;
+}
+
+int main() {
+	angka belajarcpp(3); //construktor dipanggil
+	angka* ptrbelajarcpp = new angka(5); // construktor dipanggil
+	delete ptrbelajarcpp; //destructor dipanggil
+
+	return 0;
+}//destructor dipanggil
