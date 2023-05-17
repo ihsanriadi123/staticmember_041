8fc1e0ebc8af2c9e3e51f51c0f37e99ba66cfd5b
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-17 19:07:07 +0700

    memunculkan nama mahasiswa

--- construktor func_041/construktor func_041.cpp
+++ construktor func_041/construktor func_041.cpp
@@ -37,5 +37,15 @@ void mahasiswa::cetak() {
 }
 
 int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("indra");
+	mahasiswa mhs4(30, "fauzan");
 
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
 }
