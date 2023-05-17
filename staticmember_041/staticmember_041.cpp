fc4e0239c3ab72fe5386994506e378fd70763ecb
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-17 15:25:11 +0700

    mem public static data nim mahasiswa

--- staticmember_041/staticmember_041.cpp
+++ staticmember_041/staticmember_041.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+	mahasiswa(string pnama) :nama(pnama) {setID(); }
+};
 
