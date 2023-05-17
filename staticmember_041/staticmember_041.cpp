800a98758acec122874ae77240b03b0927eadd40
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-17 15:35:23 +0700

    menampilkan data id mahasiswa

--- staticmember_041/staticmember_041.cpp
+++ staticmember_041/staticmember_041.cpp
@@ -19,3 +19,10 @@ void mahasiswa::setID() {
 	id = ++nim;
 }
 
+void mahasiswa::printALL() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << "nim = " << nim << endl;
+	cout << endl;
+}
+
