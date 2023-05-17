fb8f737c516771d049c417c3f66916984cf0987c
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-17 17:12:52 +0700

    data keseluruhan dengan menambahkan nim

--- mbrstatic func_041/mbrstatic func_041.cpp
+++ mbrstatic func_041/mbrstatic func_041.cpp
@@ -9,5 +9,12 @@ public:
 	int id;
 	string nama;
 
+	void setID();
+	void printALL();
 
+	static void setNim(int pNim) { nim = pNim; /*Definisi function*/}
+	static void getNim() { return ; /*Definisi function*/; }
+
+	mahasiswa(string pnama):nama(pnama) {setID();}
 };
+
